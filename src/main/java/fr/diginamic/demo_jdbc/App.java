933fa2f898f0c71e3d2f2f8e8dbf30964b9be1ff
Angie ANDURAND <angie-andurand@hotmail.fr>
2021-12-02 12:04:47 +0100

    premier commit

---
+++ src/main/java/fr/diginamic/demo_jdbc/App.java
@@ -0,0 +1,39 @@
+package fr.diginamic.demo_jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final String DB_URL = "jdbc:mariadb://localhost:3306/compta";
+    private static final String DB_LOGIN = "root";
+    private static final String DB_PWD = "nprspa3417";
+    
+    public static void main( String[] args )
+    {
+        try(Connection connection = DriverManager.getConnection( DB_URL, DB_LOGIN, DB_PWD )) {
+            System.out.println(connection);
+        } catch ( SQLException e ) {
+            System.err.println(e.getMessage());
+        }
+        
+        // Connection connection = null;
+        // try {
+        //     connection = DriverManager.getConnection( DB_URL, DB_LOGIN, DB_PWD );
+        //     System.out.println(connection);
+        // } catch ( SQLException throwables ) {
+        //     throwables.printStackTrace();
+        // } finally {
+        //     try {
+        //         connection.close();
+        //     } catch ( SQLException throwables ) {
+        //         throwables.printStackTrace();
+        //     }
+        // }
+    }
+}
